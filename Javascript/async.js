c96a0de3dea73de00f5ed856c421904a76d5b1cf
Shantanu Shubham <shantanu0707@gmail.com>
2023-06-09 16:51:38 +0530

    [Add] Async in callbacks

--- Javascript/async.js
+++ Javascript/async.js
@@ -1,11 +1,43 @@
-function myFunction() {
-  let a = 5;
-  let testFunction = function () {
-    a = 6;
-    console.log("This is inside setTimeout", a);
-  };
-  setTimeout(testFunction, 0);
+// function myFunction() {
+//   let a = 5;
+//   let testFunction = function () {
+//     a = 6;
+//     console.log("This is inside setTimeout", a);
+//   };
+//   setTimeout(testFunction, 0);
+//   return a;
+// }
+
+// console.log(myFunction());
+
+// this callback should be called with some data if the async code returns something
+
+function calledFunction(a, callback) {
+  function timeOutFunction() {
+    if (a < 10) {
+      // negative scenario
+      let err = new Error("a cannot be less than 10");
+      callback(err);
+    } else {
+      // positive scenario
+      callback(undefined, a * a);
+    }
+  }
+
+  setTimeout(timeOutFunction, 2 * 1000);
   return a;
 }
 
-console.log(myFunction());
+function callerFunction() {
+  let callbackFunction = (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`The returned data is: ${data}`);
+    }
+  };
+  let value = calledFunction(5, callbackFunction);
+  console.log(value);
+}
+
+callerFunction();
