20db999e583d0486396ae5c27f643f2f2d47693c
Shantanu Shubham <shantanu0707@gmail.com>
2023-06-13 16:14:53 +0530

    [Complete] Yargs

--- Node.js/first-app/app.js
+++ Node.js/first-app/app.js
@@ -12,10 +12,33 @@ function getNotes() {
   }
 }
 
+let isTitleUnique = (title, notes) => {
+  if (!notes) {
+    notes = getNotes();
+  }
+  return !notes.some((note) => note.title === title);
+};
+
 function addNewNote(note) {
   let notes = getNotes();
-  notes.push(note);
-  fs.writeFileSync(NOTE_FILE, JSON.stringify(notes));
+  if (!isTitleUnique(note.title, notes)) {
+    console.error("Title must be unique.");
+  } else {
+    notes.push(note);
+    fs.writeFileSync(NOTE_FILE, JSON.stringify(notes));
+  }
+}
+
+function deleteNoteByTitle(title, notes) {
+  if (!notes) {
+    notes = getNotes();
+  }
+  let newNotes = notes.filter((note) => note.title !== title);
+  if (newNotes.length >= notes.length) {
+    console.error("Note not found.");
+  } else {
+    fs.writeFileSync(NOTE_FILE, JSON.stringify(newNotes));
+  }
 }
 
 yargs.version("2.1.0");
@@ -41,5 +64,42 @@ yargs.command({
     addNewNote(note);
   },
 });
+
+yargs.command({
+  command: "list",
+  describe: "This is used to list all notes.",
+  handler: () => {
+    let notes = getNotes();
+    notes.forEach((note) => {
+      console.log(`NOTE:
+      Title: ${note.title}
+      Body: ${note.body}`);
+    });
+  },
+});
+
+yargs.command({
+  command: "delete",
+  describe: "This is used to delete note by title.",
+  builder: {
+    title: {
+      describe: "Please enter the title of the task you want to delete.",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: ({ title }) => {
+    deleteNoteByTitle(title);
+  },
+});
+
+/*
+  1. While adding a new note, check if the title of the note is unique.
+  If unique, add
+  Else, print -> "Change title."
+
+  2. Delete a task by given title.
+  If not note found, print "Note not found."
+*/
 
 yargs.parse();
