5c2c5f37d5e151414955e92ae35a032142da1798
Shantanu Shubham <shantanu0707@gmail.com>
2023-06-21 16:55:50 +0530

    [Add[ Update logic

--- Node.js/task-mongo-app/app.js
+++ Node.js/task-mongo-app/app.js
@@ -5,6 +5,8 @@ const User = require("./models/User");
 const app = express();
 app.use(express.json());
 
+const USER_EDITABLE_FIELDS = ["name", "age", "password"];
+
 app.post("/add-user", async (req, res) => {
   try {
     const { name, email, age, password } = req.body;
@@ -33,6 +35,39 @@ app.get("/user/:id", async (req, res) => {
 // Write an API to update a user.
 // Take info from Request Body
 // Take User ID from PATH param
+
+app.put("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const dataToUpdate = req.body;
+    let updateObject = {};
+    for (let field of USER_EDITABLE_FIELDS) {
+      if (dataToUpdate[field]) {
+        updateObject[field] = dataToUpdate[field];
+      }
+    }
+    if (Object.keys(updateObject).length) {
+      const updateResult = await User.updateOne(
+        { _id: id },
+        { $set: updateObject }
+      );
+      if (updateResult.matchedCount) {
+        console.log(
+          `Update success! User with ID: ${id} was successfully updated`
+        );
+        return res.status(200).send({
+          message: "Update Successful",
+        });
+      }
+      console.error(`Update Failed! User with ID: ${id} was not found.`);
+      return res.status(404).send({ message: "Update Failed!" });
+    } else {
+      return res.status(400).send({ message: "Please use valid fields." });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 // Task2:
 // Write an API to delete a user.
